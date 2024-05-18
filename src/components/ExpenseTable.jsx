bd920635a39faf9be69b471aa50b174f9b7f519f
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-05-18 17:34:53 -0400

    added sorting

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { useFilter } from "../hooks/useFilter";
 import ContextMenu from "./ContextMenu";
 
-
-export default function ExpenseTable({ expenses, setExpenses, setExpense, setEditingRowID }) {
+export default function ExpenseTable({
+  expenses,
+  setExpenses,
+  setExpense,
+  setEditingRowID,
+}) {
   const [filteredData, setQuery] = useFilter(expenses, (data) => data.category);
   const [menuPosition, setMenuPosition] = useState({});
   const [rowId, setRowId] = useState("");
@@ -24,7 +28,14 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
         rowId={rowId}
         setEditingRowID={setEditingRowID}
       />
-      <table className="expense-table" onClick={() => setMenuPosition({})}>
+      <table
+        className="expense-table"
+        onClick={() => {
+          if (menuPosition.left) {
+            setMenuPosition({});
+          }
+        }}
+      >
         <thead>
           <tr>
             <th>Title</th>
@@ -46,6 +57,11 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
+                  onClick={() => {
+                    setExpenses((prevState) => [
+                      ...prevState.sort((a, b) => a.amount - b.amount),
+                    ]);
+                  }}
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -55,6 +71,11 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={() => {
+                    setExpenses((prevState) => [
+                      ...prevState.sort((a, b) => b.amount - a.amount),
+                    ]);
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
