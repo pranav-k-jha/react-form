23b66cb7eceb5bc7564634be2651525c6361c5bf
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-05-18 19:13:10 -0400

    added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseTable from "./components/ExpenseTable";
 import expenseData from "./expenseData";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
   const [expenses, setExpenses] = useState(expenseData);
@@ -14,9 +15,14 @@ function App() {
 
   const [editingRowID, setEditingRowID] = useState('')
 
+  const [localData, setLocalData] = useLocalStorage('myNum', [1, 2, 3])
+  console.log(localData)
+
   return (
     <main>
-      <h1>Track Your Expense</h1>
+      <h1 onClick={() => {
+        setLocalData([4, 5, 6])
+      }}>Track Your Expense</h1>
       <div className="expense-tracker">
         <ExpenseForm
           setExpenses={setExpenses}
